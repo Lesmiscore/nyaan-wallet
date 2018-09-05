3ee8be044bf2290db36689d9109da481fe5d36fa
nao20010128nao <nao20010128@gmail.com>
2018-09-05 15:16:04 +0900

    Testnet対応のコインだけ使用

--- js/currencyList.js
+++ js/currencyList.js
@@ -19,56 +19,11 @@
 const Currency = require("./currency")
 const coinUtil = require("./coinUtil")
 const j = require("./lang").getLang() === "ja"
+const bitcoin = require("bitcoinjs-lib");
 
 // Coin id should be lowercase ticker symbol. Add prefix if this coin is different coin like testnet. Add suffix if this coin is compatible with the original coin but different mode like SegWit, Monacoin-3-Prefix
 
 const defaultCoins = [{
-  coinScreenName: j ? "モナコイン" : "Monacoin",
-  coinId: "mona",
-  unit: "MONA",
-  unitEasy: j ? "モナ" : "Mona",
-  bip44: {
-    coinType: 22,
-    account: 0
-  },
-  bip21: "monacoin",
-  defaultFeeSatPerByte: 150,
-  icon: require("../res/coins/mona.png"),
-  apiEndpoints: [{
-    url: "https://mona.monacoin.ml/insight-api-monacoin",
-    explorer: "https://mona.monacoin.ml/insight"
-  },{
-    url: "https://mona.monya.ga/insight-api-monacoin",
-    explorer: "https://mona.monya.ga/insight"
-  },{
-    url: "https://mona.insight.monaco-ex.org/insight-api-monacoin",
-    explorer: "https://mona.insight.monaco-ex.org/insight"
-  },{
-    url: "https://insight.electrum-mona.org/insight-api-monacoin",
-    explorer: "https://insight.electrum-mona.org/insight"
-  }],
-  network: {
-    messagePrefix: '\x19Monacoin Signed Message:\n',
-    bip32: {
-      public: 0x0488b21e,
-
-      private: 0x0488ade4
-    },
-    pubKeyHash: 50,
-    scriptHash: 55,
-    wif: 178, //new wif
-    bech32: "mona"
-  },
-  sound: require("../res/coins/paySound/mona.m4a"),
-  enableSegwit: false,
-  confirmations: 6,
-  counterparty:{
-    endpoints: ["https://wallet.monaparty.me/_api"],
-    nativeSymbol:"XMP"
-  },
-  opReturnLength: 83,
-  isAtomicSwapAvailable: true
-}, {
   coinScreenName: j ? "ビットゼニー" : "BitZeny",
   coinId: "zny",
   unit: "ZNY",
@@ -81,31 +36,19 @@ const defaultCoins = [{
   defaultFeeSatPerByte: 150,
   icon: require("../res/coins/zny.png"),
   apiEndpoints: [{
-    url: "https://zeny.insight.monaco-ex.org/api",
-    explorer: "https://zeny.insight.monaco-ex.org"
-  },{
-    url: "https://zenyinsight.tomotomo9696.xyz/api",
-    explorer: "https://zenyinsight.tomotomo9696.xyz"
-  }, {
-    url: "https://insight.bitzeny.jp/api",
-    explorer: "https://insight.bitzeny.jp"
-  },{
-    url: "https://insight.bitzeny.cloud/api",
-    explorer: "https://insight.bitzeny.cloud"
-  }, {
-    url: "https://zeny.fujishan.jp/api",
-    explorer: "https://zeny.fujishan.jp"
+    url: "https://test-insight.bitzeny.jp/api",
+    explorer: "https://test-insight.bitzeny.jp"
   }],
   network: {
     messagePrefix: '\x18BitZeny Signed Message:\n',
     bip32: {
-      public: 0x0488b21e,
-
-      private: 0x0488ade4
+      public: 0x043587cf,
+      private: 0x04358394
     },
-    pubKeyHash: 81,
-    scriptHash: 5,
-    wif: 128,bech32:"sz"
+    pubKeyHash: 111,
+    scriptHash: 196,
+    wif: 239,
+    bech32: "sz"
   },
   enableSegwit: false,
   sound: require("../res/coins/paySound/zny.m4a"),
@@ -124,296 +67,19 @@ const defaultCoins = [{
   defaultFeeSatPerByte: 100,
   icon: require("../res/coins/btc.png"),
   apiEndpoints: [{
-    explorer: "https://www.localbitcoinschain.com",
-    url: "https://www.localbitcoinschain.com/api"
-  },{
-    url: "https://blockexplorer.com/api",
-    explorer: "https://blockexplorer.com"
-  },{
-    explorer: "https://btc.coin.space",
-    url: "https://btc.coin.space/api"
-  },{
-    explorer: "https://explorer.bitcoin.com/btc",
-    url: "https://explorer.bitcoin.com/api/btc"
-  },{
-    url: "https://btc.blockdozer.com/insight-api",
-    explorer: "https://btc.blockdozer.com"
-  },{
-    explorer: "https://insight.bitpay.com",
-    url: "https://insight.bitpay.com/api"
-  },{
-    url: "https://btc-bitcore4.trezor.io/api",
-    explorer: "https://btc-bitcore4.trezor.io"
-  },{
-    url: "https://btc1.trezor.io/api",
-    explorer: "https://btc1.trezor.io"
-  },{
-    url: "https://core.insight.publicnodejp.cf/insight-api",
-    explorer: "https://core.insight.publicnodejp.cf/insight"
+    url: "https://tbtc.blockdozer.com/insight-api",
+    explorer: "https://tbtc.blockdozer.com"
+  }, {
+    explorer: "https://test-insight.bitpay.com",
+    url: "https://test-insight.bitpay.com/api"
   }],
-  network: {
-    messagePrefix: '\x18Bitcoin Signed Message:\n',
-    bip32: {
-      public: 0x0488b21e,
-
-      private: 0x0488ade4
-    },
-    pubKeyHash: 0,
-    scriptHash: 5,
-    bech32: "bc",
-    wif: 128
-  },
+  network: bitcoin.networks.testnet,
   enableSegwit: false,
   confirmations: 6,
-  counterparty:{endpoints: ["https://wallet.counterwallet.io/_api"],nativeSymbol:"XCP"},
-  opReturnLength: 83,
-  isAtomicSwapAvailable: true
-}, {
-  coinScreenName: j ? "ライトコイン" : "Litecoin",
-  coinId: "ltc",
-  unit: "LTC",
-  unitEasy: j ? "ライトコイン" : "Litecoin",
-  bip44: {
-    coinType: 2,
-    account: 0
+  counterparty: {
+    endpoints: ["https://wallet.counterwallet.io/_api"],
+    nativeSymbol: "XCP"
   },
-  bip21: "litecoin",
-  defaultFeeSatPerByte: 300,
-  icon: require("../res/coins/ltc.png"),
-  apiEndpoints: [{
-    url: "https://insight.litecore.io/api",
-    explorer: "https://insight.litecore.io"
-  },{
-    explorer: "https://ltc.coin.space",
-    url: "https://ltc.coin.space/api"
-  },{
-    url: "https://ltc-bitcore1.trezor.io/api",
-    explorer: "https://ltc-bitcore1.trezor.io"
-  },{
-    url:"https://ltc.insight.publicnodejp.cf/insight-lite-api",
-    explorer:"https://ltc.insight.publicnodejp.cf/insight"
-  }],
-  network: {
-    messagePrefix: '\x19Litecoin Signed Message:\n',
-    bip32: {
-      public: 0x0488b21e,
-
-      private: 0x0488ade4
-    },
-    pubKeyHash: 48,
-    scriptHash: 5,
-    wif: 176,
-    bech32: "ltc"
-  },
-  enableSegwit: false,
-  confirmations: 6,
-  opReturnLength: 83,
-  isAtomicSwapAvailable: true
-}, {
-  coinScreenName: j ? "フジコイン" : "FujiCoin",
-  coinId: "fjc",
-  unit: "FJC",
-  unitEasy: j ? "フジコイン" : "FujiCoin",
-  bip44: {
-    coinType: 75,
-    account: 0
-  },
-  bip21: "fujicoin",
-  defaultFeeSatPerByte: 200,
-  icon: require("../res/coins/fjc.png"),
-  apiEndpoints: [{
-    url: "http://explorer.fujicoin.org/api",
-    explorer: "http://explorer.fujicoin.org",
-    proxy: true
-  }],
-  network: {
-    messagePrefix: '\x19Fujicoin Signed Message:\n',
-    bip32: {
-      public: 0x0488b21e,
-      private: 0x0488ade4
-    },
-    pubKeyHash: 36,
-    scriptHash: 16,
-    wif: 164
-  },
-  enableSegwit: false,
-  confirmations: 6,
-  opReturnLength: 83,
-  isAtomicSwapAvailable: true
-}, {
-  coinScreenName: j ? "ビットコイン(SegWit)" : "Bitcoin(SegWit)",
-  coinId: "btcsw",
-  unit: "BTC(SW)",
-  unitEasy: j ? "ビットコイン(SW)" : "Bitcoin(SW)",
-  bip49: {
-    coinType: 0,
-    account: 0
-  },
-  bip21: "bitcoin",
-  defaultFeeSatPerByte: 100,
-  icon: require("../res/coins/btcsw.png"),
-  apiEndpoints: [{
-    url: "https://blockexplorer.com/api",
-    explorer: "https://blockexplorer.com"
-  },{
-    explorer: "https://explorer.bitcoin.com/btc",
-    url: "https://explorer.bitcoin.com/api/btc"
-  },{
-    explorer: "https://insight.bitpay.com",
-    url: "https://insight.bitpay.com/api"
-  },{
-    explorer: "https://btc.coin.space",
-    url: "https://btc.coin.space/api"
-  },{
-    url: "https://btc-bitcore4.trezor.io/api",
-    explorer: "https://btc-bitcore4.trezor.io"
-  },{
-    url: "https://btc1.trezor.io/api",
-    explorer: "https://btc1.trezor.io"
-  },{
-    url: "https://btc.blockdozer.com/insight-api",
-    explorer: "https://btc.blockdozer.com"
-  },{
-    url: "https://core.insight.publicnodejp.cf/insight-api",
-    explorer: "https://core.insight.publicnodejp.cf/insight"
-  },{
-    explorer: "https://www.localbitcoinschain.com",
-    url: "https://www.localbitcoinschain.com/api"
-  }],
-  network: {
-    messagePrefix: '\x18Bitcoin Signed Message:\n',
-    bip32: {
-      public: 0x0488b21e,
-
-      private: 0x0488ade4
-    },
-    pubKeyHash: 0, // 1
-    scriptHash: 5, // 3
-    bech32: "bc",
-    wif: 128
-  },
-  enableSegwit: "legacy",
-  confirmations: 6,
-  counterparty:{endpoints: ["https://wallet.counterwallet.io/_api"],nativeSymbol:"XCP"},
-  opReturnLength: 83,
-  isAtomicSwapAvailable: false
-}, {
-  coinScreenName: j ? "ビットコインキャッシュ" : "Bitcoin Cash",
-  coinId: "bch",
-  unit: "BCH",
-  unitEasy: j ? "ビッチ" : "BitCh",
-  bip44: {
-    coinType: 145,
-    account: 0
-  },
-  bip21: "bitcoincash",
-  defaultFeeSatPerByte: 100,
-  icon: require("../res/coins/bch.png"),
-  apiEndpoints: [{
-    explorer: "https://bitcoincash.blockexplorer.com",
-    url: "https://bitcoincash.blockexplorer.com/api"
-  },{
-    explorer: "https://bch.coin.space",
-    url: "https://bch.coin.space/api"
-  },{
-    explorer: "https://explorer.bitcoin.com/bch", 
-    url: "https://explorer.bitcoin.com/api/bch"
- },{
-    explorer: "https://cash.insight.publicnodejp.cf/insight",
-    url: "https://cash.insight.publicnodejp.cf/insight-api"
-  }],
-  network: {
-    messagePrefix: '\x18Bitcoin Signed Message:\n',
-    bip32: {
-      public: 0x0488b21e,
-
-      private: 0x0488ade4
-    },
-    pubKeyHash: 0, // 1
-    scriptHash: 5, // 3
-    wif: 128
-  },
-  enableSegwit: false,
-  lib: "bch",
-  confirmations: 6,
-  opReturnLength: 83, // change after hard fork,
-  isAtomicSwapAvailable: true
-}, {
-  "coinScreenName": j ? "コト" : "Koto",
-  "coinId": "koto",
-  "unit": "KOTO",
-  "unitEasy": j ? "コト" : "Koto",
-  "bip44": {
-    "coinType": 2001,
-    "account": 0
-  },
-  "bip21": "koto",
-  "defaultFeeSatPerByte": 200,
-  "icon": require("../res/coins/koto.png"),
-  "network": {
-    "messagePrefix": "\u0015Koto Signed Message:\n",
-    "bip32": {
-      "public": 76067358,
-      "private": 76066276
-    },
-    "pubKeyHash": 6198,
-    "scriptHash": 6203,
-    "txversion": 3,
-    "versionGroupId": 48748912,
-    "wif": 128
-  },
-  "enableSegwit": false,
-  "confirmations": 6,
-  "lib": "zec",
-  "apiEndpoints": [{
-    "url": "https://koto.monacoin.ml/insight-api-koto",
-    "explorer": "https://koto.monacoin.ml/insight"
-  },{
-    "url": "https://insight.kotocoin.info/api",
-    "explorer": "https://insight.kotocoin.info"
-  }],
-  opReturnLength: 80,
-  isAtomicSwapAvailable: true
-}, {
-  "coinScreenName": j ? "ダッシュ" : "Dash",
-  "coinId": "dash",
-  "unit": "DASH",
-  "unitEasy": j ? "ダッシュ" : "Dash",
-  "bip44": {
-    "coinType": 5,
-    "account": 0
-  },
-  "bip21": "dash",
-  "defaultFeeSatPerByte": 200,
-  "icon": require("../res/coins/dash.png"),
-  "network": {
-    "messagePrefix": "\u0019DarkCoin Signed Message:\n",
-    "bip32": {
-      "public": 76067358,
-      "private": 76066276
-    },
-    "pubKeyHash": 76,
-    "scriptHash": 16,
-    "wif": 204
-  },
-  "enableSegwit": false,
-  "confirmations": 6,
-  "apiEndpoints": [{
-    url: "https://insight.dash.org/insight-api-dash",
-    explorer: "https://insight.dash.org/insight"
-  },{
-    url: "https://insight.dash.siampm.com/api",
-    explorer: "https://insight.dash.siampm.com"
-  },{
-    "url": "https://dash-bitcore1.trezor.io/api",
-    "explorer": "https://dash-bitcore1.trezor.io"
-  },{
-    "url": "https://dash-bitcore2.trezor.io/api",
-    "explorer": "https://dash-bitcore1.trezor.io"
-  },{
-    "url": "https://dash-bitcore3.trezor.io/api",
-    "explorer": "https://dash-bitcore1.trezor.io"
-  }],
   opReturnLength: 83,
   isAtomicSwapAvailable: true
 }, {
@@ -431,102 +97,23 @@ const defaultCoins = [{
   "network": {
     "messagePrefix": "\u0016Zcash Signed Message:\n",
     "bip32": {
-      "public": 76067358,
-      "private": 76066276
+      "public": 0x043587cf,
+      "private": 0x04358394
     },
-    "pubKeyHash": 7352,
-    "scriptHash": 7357,
+    "pubKeyHash": 0x1d25,
+    "scriptHash": 0x1cba,
     "txversion": 3,
     "versionGroupId": 63210096,
-    "wif": 128
+    "wif": 0xef
   },
   "apiEndpoints": [{
-    "url": "https://zcash.blockexplorer.com/api",
-    "explorer": "https://zcash.blockexplorer.com"
-  },{
-    url: "https://zcashnetwork.info/api",
-    explorer: "https://zcashnetwork.info"
-  },{
-    url: "https://explorer.zcashfr.io/api",
-    explorer: "https://explorer.zcashfr.io"
-  },{
-    "url": "https://zec-bitcore1.trezor.io/api",
-    "explorer": "https://zec-bitcore1.trezor.io"
-  },{
-    "url": "https://zec-bitcore3.trezor.io/api",
-    "explorer": "https://zec-bitcore1.trezor.io"
-  },{
-    "url": "https://zec-bitcore2.trezor.io/api",
-    "explorer": "https://zec-bitcore1.trezor.io"
+    "url": "https://explorer.testnet.z.cash/api",
+    "explorer": "https://explorer.testnet.z.cash"
   }],
   lib: "zec",
   opReturnLength: 80,
   isAtomicSwapAvailable: true
-}, {
-  coinScreenName: j ? "野獣コイン" : "Yajucoin",
-  coinId: "yaju",
-  unit: "YAJU",
-  unitEasy: "¥",
-  bip44: {
-    coinType: 2005,
-    account: 0
-  },
-  bip21: "yajucoin",
-  defaultFeeSatPerByte: 50,
-  icon: require("../res/coins/yaju.png"),
-  apiEndpoints: [{
-    url: "https://yaju.monacoin.ml/api",
-    explorer: "https://yaju.monacoin.ml"
-  },{
-    url: "https://yaju.insight.publicnodejp.cf/api",
-    explorer: "https://yaju.monacoin.ml"
-  }],
-  network: {
-    messagePrefix: '\x19YAJUCOIN Signed Message:\n',
-    bip32: {
-      public: 0x0488b21e,
-      private: 0x0488ade4
-    },
-    pubKeyHash: 78,
-    scriptHash: 85, // n
-    wif: 206
-  },
-  enableSegwit: false,
-  lib: "blk",
-  opReturnLength: 0,
-  isAtomicSwapAvailable: false
-},{
-  coinScreenName: j ? "NEETCOIN" : "NeetCoin",
-  coinId: "neet",
-  unit: "NEET",
-  unitEasy: j ? "ニート" : "Neet",
-  bip44: {
-    coinType: 2002,
-    account: 0
-  },
-  bip21: "neetcoin",
-  defaultFeeSatPerByte: 50,
-  icon: require("../res/coins/neet.png"),
-  apiEndpoints: [{
-    url: "https://insight.neetcoin.jp/api",
-    explorer: "https://insight.neetcoin.jp"
-  }],
-  network: {
-    messagePrefix: '\x19NEETCOIN Signed Message:\n',
-    bip32: {
-      public: 0x0488b21e,
-      private: 0x0488ade4
-    },
-    pubKeyHash: 53, // N
-    scriptHash: 112, // n
-    wif: 181
-  },
-  enableSegwit: false,
-  lib: "blk",
-  opReturnLength: 0,
-  isAtomicSwapAvailable: false
-}
-                     ]
+}]
 
 
 const coins = {}
